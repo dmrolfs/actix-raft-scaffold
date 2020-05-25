89330102bcb96e448861ece8cf6eb1e920bac5bc
Damon Rolfs <drolfs@gmail.com>
2020-05-24 22:27:50 -0700

    initial refactor stage wrt localnode

--- src/raft/network/proximity.rs
+++ src/raft/network/proximity.rs
@@ -3,24 +3,25 @@ use actix_raft::{AppData, AppDataResponse, AppError, RaftStorage};
 use actix_raft::messages as raft_protocol;
 use tracing::*;
 use crate::network::node::{Node, LocalNode, RemoteNode};
+use crate::raft::Raft;
 use crate::ports::http::entities as port_entities;
 use serde::{Serialize, Deserialize};
 
 pub trait RaftProtocolBehavior<D: AppData> {
     fn append_entries(
         &self,
-        msg: raft_protocol::AppendEntriesRequest<D>,
+        rpc: raft_protocol::AppendEntriesRequest<D>,
         ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::AppendEntriesResponse, Error = ()>>;
     fn install_snapshot(
         &self,
-        msg: raft_protocol::InstallSnapshotRequest,
+        rpc: raft_protocol::InstallSnapshotRequest,
         ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::InstallSnapshotResponse, Error = ()>>;
 
     fn vote(
         &self,
-        msg: raft_protocol::VoteRequest,
+        rpc: raft_protocol::VoteRequest,
         ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::VoteResponse, Error = ()>>;
 }
@@ -33,85 +34,125 @@ impl<D, R, E, S> RaftProtocolBehavior<D> for LocalNode<D, R, E, S>
         E: AppError,
         S: RaftStorage<D, R, E>,
 {
-    #[tracing::instrument(skip(self, msg, _ctx))]
+    #[tracing::instrument(skip(self, rpc, _ctx))]
     fn append_entries(
         &self,
-        msg: raft_protocol::AppendEntriesRequest<D>,
+        rpc: raft_protocol::AppendEntriesRequest<D>,
         _ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::AppendEntriesResponse, Error = ()>> {
-        debug!(
-            proximity = ?self, raft_command = "AppendEntries",
-            "Submitting Raft RPC to local Raft."
-        );
-
         Box::new(
-            fut::wrap_future::<_, Node<D>>(self.raft.send(msg))
-                .map_err(|err, n, _| {
-                    error!(
-                        local_id = n.local_id, proximity = ?n.proximity, error = ?err,
-                        "Raft AppendEntries RPC failed in actor send."
-                    );
-                    ()
-                })
+            fut::wrap_future::<_, Node<D>>(self.submit_to_raft(rpc))
                 .and_then(|res, _, _| fut::result(res))
         )
+        // debug!(
+        //     proximity = ?self, raft_command = "AppendEntries",
+        //     "Submitting Raft RPC to local Raft."
+        // );
+        //
+        // Box::new(
+        //     fut::wrap_future::<_, Node<D>>(self.raft.send(msg))
+        //         .map_err(|err, n, _| {
+        //             error!(
+        //                 local_id = n.local_id, proximity = ?n.proximity, error = ?err,
+        //                 "Raft AppendEntries RPC failed in actor send."
+        //             );
+        //             ()
+        //         })
+        //         .and_then(|res, _, _| fut::result(res))
+        // )
     }
 
-    #[tracing::instrument(skip(self, msg, _ctx))]
+    #[tracing::instrument(skip(self, rpc, _ctx))]
     fn install_snapshot(
         &self,
-        msg: raft_protocol::InstallSnapshotRequest,
+        rpc: raft_protocol::InstallSnapshotRequest,
         _ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::InstallSnapshotResponse, Error = ()>> {
-        debug!(
-            proximity = ?self, raft_command = "InstallSnapshot",
-            "Submitting Raft RPC to local Raft."
-        );
-
         Box::new(
-            fut::wrap_future::<_, Node<D>>(self.raft.send(msg))
-                .map_err(|err, n, _| {
-                    error!(
-                        local_id = n.local_id, proximity = ?n.proximity, error = ?err,
-                        "Raft AppendEntries RPC failed in actor send."
-                    );
-                    ()
-                })
+            fut::wrap_future::<_, Node<D>>(self.submit_to_raft(rpc))
                 .and_then(|res, _, _| fut::result(res))
         )
+        // debug!(
+        //     proximity = ?self, raft_command = "InstallSnapshot",
+        //     "Submitting Raft RPC to local Raft."
+        // );
+        //
+        // Box::new(
+        //     fut::wrap_future::<_, Node<D>>(self.raft.send(rpc))
+        //         .map_err(|err, n, _| {
+        //             error!(
+        //                 local_id = n.local_id, proximity = ?n.proximity, error = ?err,
+        //                 "Raft AppendEntries RPC failed in actor send."
+        //             );
+        //             ()
+        //         })
+        //         .and_then(|res, _, _| fut::result(res))
+        // )
     }
 
-    #[tracing::instrument(skip(self, msg, _ctx))]
+    #[tracing::instrument(skip(self, rpc, _ctx))]
     fn vote(
         &self,
-        msg: raft_protocol::VoteRequest,
+        rpc: raft_protocol::VoteRequest,
         _ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::VoteResponse, Error = ()>> {
-        debug!(
-            proximity = ?self, raft_command = "VoteRequest",
-            "Submitting Raft RPC to local Raft."
-        );
+        Box::new(
+            fut::wrap_future::<_, Node<D>>(self.submit_to_raft(rpc))
+                .and_then(|res, _, _| fut::result(res))
+        )
+        // debug!(
+        //     proximity = ?self, raft_command = "VoteRequest",
+        //     "Submitting Raft RPC to local Raft."
+        // );
+        //
+        // Box::new(
+        //     fut::wrap_future::<_, Node<D>>(self.raft.send(rpc))
+        //         .map_err(|err, n, _| {
+        //             error!(
+        //                 local_id = n.local_id, proximity = ?n.proximity, error = ?err,
+        //                 "Raft VoteRequest RPC failed in actor send."
+        //             );
+        //             ()
+        //         })
+        //         .and_then(|res, _, _| fut::result(res))
+        // )
+    }
+}
+
+impl<D, R, E, S> LocalNode<D, R, E, S>
+    where
+        D: AppData,
+        R: AppDataResponse,
+        E: AppError,
+        S: RaftStorage<D, R, E>,
+{
+    #[tracing::instrument(skip(self))]
+    fn submit_to_raft<C>(&self, rpc: C) -> Box<dyn Future<Item = C::Result, Error = ()>>
+    where
+        C: Message + Send + std::fmt::Debug + 'static,
+        C::Result: Send,
+        Raft<D, R, E, S>: Handler<C>,
+        <Raft<D, R, E, S> as Actor>::Context: actix::dev::ToEnvelope<Raft<D, R, E, S>, C>
+    {
+        let local_id = self.id;
+        let proximity = self.clone();
+        debug!(?proximity, raft_rpc = ?rpc, "Submitting Raft RPC to local Raft.");
 
         Box::new(
-            fut::wrap_future::<_, Node<D>>(self.raft.send(msg))
-                .map_err(|err, n, _| {
-                    error!(
-                        local_id = n.local_id, proximity = ?n.proximity, error = ?err,
-                        "Raft VoteRequest RPC failed in actor send."
-                    );
+            self.raft.send(rpc)
+                .map_err(move |err| {
+                    error!(local_id, ?proximity, error = ?err,"Raft RPC failed in actor send.");
                     ()
                 })
-                .and_then(|res, _, _| fut::result(res))
         )
     }
 }
 
-
 impl<D: AppData> RaftProtocolBehavior<D> for RemoteNode {
-    #[tracing::instrument(skip(self, msg, ctx))]
+    #[tracing::instrument(skip(self, rpc, ctx))]
     fn append_entries(
         &self,
-        msg: raft_protocol::AppendEntriesRequest<D>,
+        rpc: raft_protocol::AppendEntriesRequest<D>,
         ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::AppendEntriesResponse, Error = ()>> {
         self.send_raft_command::<
@@ -120,13 +161,13 @@ impl<D: AppData> RaftProtocolBehavior<D> for RemoteNode {
             raft_protocol::AppendEntriesResponse,
             port_entities::RaftAppendEntriesRequest,
             port_entities::RaftAppendEntriesResponse
-        >(msg, "entries", ctx)
+        >(rpc, "entries", ctx)
     }
 
-    #[tracing::instrument(skip(self, msg, ctx))]
+    #[tracing::instrument(skip(self, rpc, ctx))]
     fn install_snapshot(
         &self,
-        msg: raft_protocol::InstallSnapshotRequest,
+        rpc: raft_protocol::InstallSnapshotRequest,
         ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::InstallSnapshotResponse, Error = ()>> {
         self.send_raft_command::<
@@ -135,13 +176,13 @@ impl<D: AppData> RaftProtocolBehavior<D> for RemoteNode {
             raft_protocol::InstallSnapshotResponse,
             port_entities::RaftInstallSnapshotRequest,
             port_entities::RaftInstallSnapshotResponse
-        >(msg, "snapshots", ctx)
+        >(rpc, "snapshots", ctx)
     }
 
-    #[tracing::instrument(skip(self, msg, ctx))]
+    #[tracing::instrument(skip(self, rpc, ctx))]
     fn vote(
         &self,
-        msg: raft_protocol::VoteRequest,
+        rpc: raft_protocol::VoteRequest,
         ctx: &mut <Node<D> as Actor>::Context
     ) -> Box<dyn ActorFuture<Actor = Node<D>, Item = raft_protocol::VoteResponse, Error = ()>> {
         self.send_raft_command::<
@@ -150,7 +191,7 @@ impl<D: AppData> RaftProtocolBehavior<D> for RemoteNode {
             raft_protocol::VoteResponse,
             port_entities::RaftVoteRequest,
             port_entities::RaftVoteResponse
-        >(msg, "vote", ctx)
+        >(rpc, "vote", ctx)
     }
 }
 
@@ -182,6 +223,7 @@ impl RemoteNode {
             .send()
             .and_then(|mut resp| resp.json::<R0>())
             .map_err(|err| {
+                //todo handle redirect, which indicates leader, but this should be reflected in raft_metrics and adjusted via consensus
                 self.log_raft_protocol_error(err);
                 ()
             })
